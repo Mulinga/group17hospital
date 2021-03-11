8da152ec5bf040d1060a3af070f7ede9fe326c5e
melissa-koi <melissawangui3@gmail.com>
2021-03-12 01:48:46 +0300

    cleaned up some code

--- assets/js/addPatients.js
+++ assets/js/addPatients.js
@@ -1,162 +1,156 @@
 
+// User interface logic
+// Reset Function
+function resetValues() {
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#email").val("");
+    $("#phoneNumber").val("");
+    $("#emergencyphoneNumber").val("");
+    $("#birthday").val("");
+    $("#gender").val("");
+    $("input#inpatientFirstName").val("");
+    $("#inpatientLastName").val("");
+    $("#inpatientEmail").val("");
+    $("#inpatientPhoneNumber").val("");
+    $("#inpatientEmergencyphoneNumber").val("");
+    $("#inpatientBirthday").val("");
+    $("#inpatientGender").val("");
+};
+$("button.outPatient").click(function (event) {
+    event.preventDefault();
+    $("form#outpatientForm").slideToggle();
+});
+$("#outpatientRegister").click(function (event) {
+    event.preventDefault();
+    let inputtedFirstname = $("#firstName").val();
+    let inputtedLastname = $("#lastName").val();
+    let inputtedEmail = $("#email").val();
+    let inputtedPhoneNumber = $("#phoneNumber").val();
+    let inputtedEmergencyphoneNumber = $("#emergencyphoneNumber").val();
+    let inputtedBirthday = $("#birthday").val();
+    let inputtedGender = $("#gender").val();
 
-//$(document).ready(function () {
-//     console.log("Content loaded");
+    // console.log("Outpatient firstname: ",inputtedFirstname);
+    // console.log("Outpatient lastName: ",inputtedLastname);
+    // console.log("Outpatient email: ",inputtedEmail);
+    // console.log("Outpatient phoneNumber: ",inputtedPhoneNumber);
+    // console.log("Outpatient emergencyphoneNumber: ",inputtedEmergencyphoneNumber);
+    // console.log("Outpatient birthday: ",inputtedBirthday);
+    // console.log("Outpatient gender: ",inputtedGender);
 
-    // User interface logic
-    // Reset Function
-    function resetValues() {
-        $("#firstName").val("");
-        $("#lastName").val("");
-        $("#email").val("");
-        $("#phoneNumber").val("");
-        $("#emergencyphoneNumber").val("");
-        $("#birthday").val("");
-        $("#gender").val("");
-        $("input#inpatientFirstName").val("");
-        $("#inpatientLastName").val("");
-        $("#inpatientEmail").val("");
-        $("#inpatientPhoneNumber").val("");
-        $("#inpatientEmergencyphoneNumber").val("");
-        $("#inpatientBirthday").val("");
-        $("#inpatientGender").val("");
-    };
-    $("button.outPatient").click(function (event) {
-        event.preventDefault();
-        $("form#outpatientForm").slideToggle();
-    });
-    $("#outpatientRegister").click(function (event) {
-        event.preventDefault();
-        let inputtedFirstname = $("#firstName").val();
-        let inputtedLastname = $("#lastName").val();
-        let inputtedEmail = $("#email").val();
-        let inputtedPhoneNumber = $("#phoneNumber").val();
-        let inputtedEmergencyphoneNumber = $("#emergencyphoneNumber").val();
-        let inputtedBirthday = $("#birthday").val();
-        let inputtedGender = $("#gender").val();
+    if (inputtedFirstname == '' || inputtedLastname == '' || inputtedEmail == '' || inputtedPhoneNumber == '' || inputtedEmergencyphoneNumber == '' || inputtedBirthday == '' || inputtedGender == '') {
+        alert("Please fill all the fields!")
+    } else {
+        $("#outpatientDetails").show();
 
-        // console.log("Outpatient firstname: ",inputtedFirstname);
-        // console.log("Outpatient lastName: ",inputtedLastname);
-        // console.log("Outpatient email: ",inputtedEmail);
-        // console.log("Outpatient phoneNumber: ",inputtedPhoneNumber);
-        // console.log("Outpatient emergencyphoneNumber: ",inputtedEmergencyphoneNumber);
-        // console.log("Outpatient birthday: ",inputtedBirthday);
-        // console.log("Outpatient gender: ",inputtedGender);
+        // Appending to the table
+        var tableRow = document.createElement("tr");
 
-        if (inputtedFirstname == '' || inputtedLastname == '' || inputtedEmail == '' || inputtedPhoneNumber == '' || inputtedEmergencyphoneNumber == '' || inputtedBirthday == '' || inputtedGender == '') {
-            alert("Please fill all the fields!")
-        } else {
-            $("#outpatientDetails").show();
+        var tabledataFName = document.createElement("td");
+        tabledataFName.innerHTML = inputtedFirstname;
+        
+        var tabledataLName = document.createElement("td");
+        tabledataLName.innerHTML = inputtedLastname;
 
-            // Appending to the table
-            var tableRow = document.createElement("tr");
+        var tabledataEmail = document.createElement("td");
+        tabledataEmail.innerHTML = inputtedEmail;
 
-            var tabledataFName = document.createElement("td");
-            tabledataFName.innerHTML = inputtedFirstname;
-            
-            var tabledataLName = document.createElement("td");
-            tabledataLName.innerHTML = inputtedLastname;
+        var tabledataPhone = document.createElement("td");
+        tabledataPhone.innerHTML = inputtedPhoneNumber;
 
-            var tabledataEmail = document.createElement("td");
-            tabledataEmail.innerHTML = inputtedEmail;
+        var tabledataEmPhone = document.createElement("td");
+        tabledataEmPhone.innerHTML = inputtedEmergencyphoneNumber;
 
-            var tabledataPhone = document.createElement("td");
-            tabledataPhone.innerHTML = inputtedPhoneNumber;
+        var tabledataDOB = document.createElement("td");
+        tabledataDOB.innerHTML = inputtedBirthday;
 
-            var tabledataEmPhone = document.createElement("td");
-            tabledataEmPhone.innerHTML = inputtedEmergencyphoneNumber;
-
-            var tabledataDOB = document.createElement("td");
-            tabledataDOB.innerHTML = inputtedBirthday;
-
-            var tabledataSex = document.createElement("td");
-            tabledataSex.innerHTML = inputtedGender;
+        var tabledataSex = document.createElement("td");
+        tabledataSex.innerHTML = inputtedGender;
 
 
-            tableRow.appendChild(tabledataFName);
-            tableRow.appendChild(tabledataLName);
-            tableRow.appendChild(tabledataEmail);
-            tableRow.appendChild(tabledataPhone);
-            tableRow.appendChild(tabledataEmPhone);
-            tableRow.appendChild(tabledataDOB);
-            tableRow.appendChild(tabledataSex);
+        tableRow.appendChild(tabledataFName);
+        tableRow.appendChild(tabledataLName);
+        tableRow.appendChild(tabledataEmail);
+        tableRow.appendChild(tabledataPhone);
+        tableRow.appendChild(tabledataEmPhone);
+        tableRow.appendChild(tabledataDOB);
+        tableRow.appendChild(tabledataSex);
 
-            $("#outpatientBody").append(tableRow);
+        $("#outpatientBody").append(tableRow);
 
-            resetValues();
+        resetValues();
 
-        }
-    });
+    }
+});
 
 
 
-    $("button.inPatient").click(function (event) {
-        event.preventDefault();
-        $("form#inpatientForm").slideToggle();
-    });
-    $("#inpatientRegister").click(function (event) {
-        event.preventDefault()
+$("button.inPatient").click(function (event) {
+    event.preventDefault();
+    $("form#inpatientForm").slideToggle();
+});
+$("#inpatientRegister").click(function (event) {
+    event.preventDefault()
 
-        let inputtedInpatientFirstName = $("#inpatientFirstName").val();
-        let inputtedInpatientLastname = $("#inpatientLastName").val();
-        let inputtedInpatientEmail = $("#inpatientEmail").val();
-        let inputtedInpatientPhoneNumber = $("#inpatientPhoneNumber").val();
-        let inputtedInpatientEmergencyphoneNumber = $("#inpatientEmergencyphoneNumber").val();
-        let inputtedInpatientBirthday = $("#inpatientBirthday").val();
-        let inputteInpatientdGender = $("#inpatientGender").val();
-
-
-        // console.log("Outpatient firstname: ", inputtedInpatientFirstName);
-        // console.log("Outpatient lastName: ", inputtedInpatientLastname);
-        // console.log("Outpatient email: ", inputtedInpatientEmail);
-        // console.log("Outpatient phoneNumber: ", inputtedInpatientPhoneNumber);
-        // console.log("Outpatient emergencyphoneNumber: ", inputtedInpatientEmergencyphoneNumber);
-        // console.log("Outpatient birthday: ", inputtedInpatientBirthday);
-        // console.log("Outpatient gender: ", inputteInpatientdGender);
-
-        if (inputtedInpatientFirstName == '' || inputtedInpatientLastname == '' || inputtedInpatientEmail == '' || inputtedInpatientPhoneNumber == '' || inputtedInpatientEmergencyphoneNumber == '' || inputtedInpatientBirthday == '' || inputteInpatientdGender == '') {
-            alert("Please fill all the fields!")
-        }else{
-            $("#inpatientDetails").show();
-
-            // Appending to the table
-            var tableRow = document.createElement("tr");
-
-            var InpatientTabledataFName = document.createElement("td");
-            InpatientTabledataFName.innerHTML = inputtedInpatientFirstName;
-
-            var InpatientTabledataLName = document.createElement("td");
-            InpatientTabledataLName.innerHTML = inputtedInpatientLastname;
-
-            var InpatientTabledataEmail = document.createElement("td");
-            InpatientTabledataEmail.innerHTML = inputtedInpatientEmail;
-
-            var InpatientTabledataPhone = document.createElement("td");
-            InpatientTabledataPhone.innerHTML = inputtedInpatientPhoneNumber;
-
-            var InpatientTabledataEmPhone = document.createElement("td");
-            InpatientTabledataEmPhone.innerHTML = inputtedInpatientEmergencyphoneNumber;
-
-            var InpatientTabledataDOB = document.createElement("td");
-            InpatientTabledataDOB.innerHTML = inputtedInpatientBirthday;
-
-            var InpatientTabledataSex = document.createElement("td");
-            InpatientTabledataSex.innerHTML = inputteInpatientdGender;
+    let inputtedInpatientFirstName = $("#inpatientFirstName").val();
+    let inputtedInpatientLastname = $("#inpatientLastName").val();
+    let inputtedInpatientEmail = $("#inpatientEmail").val();
+    let inputtedInpatientPhoneNumber = $("#inpatientPhoneNumber").val();
+    let inputtedInpatientEmergencyphoneNumber = $("#inpatientEmergencyphoneNumber").val();
+    let inputtedInpatientBirthday = $("#inpatientBirthday").val();
+    let inputteInpatientdGender = $("#inpatientGender").val();
 
 
-            tableRow.appendChild(InpatientTabledataFName);
-            tableRow.appendChild(InpatientTabledataLName);
-            tableRow.appendChild(InpatientTabledataEmail);
-            tableRow.appendChild(InpatientTabledataPhone);
-            tableRow.appendChild(InpatientTabledataEmPhone);
-            tableRow.appendChild(InpatientTabledataDOB);
-            tableRow.appendChild(InpatientTabledataSex);
+    // console.log("Outpatient firstname: ", inputtedInpatientFirstName);
+    // console.log("Outpatient lastName: ", inputtedInpatientLastname);
+    // console.log("Outpatient email: ", inputtedInpatientEmail);
+    // console.log("Outpatient phoneNumber: ", inputtedInpatientPhoneNumber);
+    // console.log("Outpatient emergencyphoneNumber: ", inputtedInpatientEmergencyphoneNumber);
+    // console.log("Outpatient birthday: ", inputtedInpatientBirthday);
+    // console.log("Outpatient gender: ", inputteInpatientdGender);
 
-            $("#inpatientTbody").append(tableRow);
+    if (inputtedInpatientFirstName == '' || inputtedInpatientLastname == '' || inputtedInpatientEmail == '' || inputtedInpatientPhoneNumber == '' || inputtedInpatientEmergencyphoneNumber == '' || inputtedInpatientBirthday == '' || inputteInpatientdGender == '') {
+        alert("Please fill all the fields!")
+    }else{
+        $("#inpatientDetails").show();
 
-            resetValues();
+        // Appending to the table
+        var tableRow = document.createElement("tr");
 
-            };
-        })
-    // });
-// });
+        var InpatientTabledataFName = document.createElement("td");
+        InpatientTabledataFName.innerHTML = inputtedInpatientFirstName;
+
+        var InpatientTabledataLName = document.createElement("td");
+        InpatientTabledataLName.innerHTML = inputtedInpatientLastname;
+
+        var InpatientTabledataEmail = document.createElement("td");
+        InpatientTabledataEmail.innerHTML = inputtedInpatientEmail;
+
+        var InpatientTabledataPhone = document.createElement("td");
+        InpatientTabledataPhone.innerHTML = inputtedInpatientPhoneNumber;
+
+        var InpatientTabledataEmPhone = document.createElement("td");
+        InpatientTabledataEmPhone.innerHTML = inputtedInpatientEmergencyphoneNumber;
+
+        var InpatientTabledataDOB = document.createElement("td");
+        InpatientTabledataDOB.innerHTML = inputtedInpatientBirthday;
+
+        var InpatientTabledataSex = document.createElement("td");
+        InpatientTabledataSex.innerHTML = inputteInpatientdGender;
+
+
+        tableRow.appendChild(InpatientTabledataFName);
+        tableRow.appendChild(InpatientTabledataLName);
+        tableRow.appendChild(InpatientTabledataEmail);
+        tableRow.appendChild(InpatientTabledataPhone);
+        tableRow.appendChild(InpatientTabledataEmPhone);
+        tableRow.appendChild(InpatientTabledataDOB);
+        tableRow.appendChild(InpatientTabledataSex);
+
+        $("#inpatientTbody").append(tableRow);
+
+        resetValues();
+
+        };
+    })
