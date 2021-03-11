42ac4cbded7e9dfacf6fbbc65ab5fc0e4f3a66e2
Paul-Ngigi <paulkush7777@gmail.com>
2021-03-11 12:26:36 +0300

    update the appointment section

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log("Content loaded");
 
     // User interface logic
@@ -9,81 +9,77 @@ $(document).ready(function() {
         $("#email").val("");
         $("#yourNumber").val("");
         $("#emergencyContact").val("");
-        $("#department").val("");
-        $("#doctor").val("");
         $("#appointmentDate").val("");
-        $("#clientFirstName").val("");
-        $("#clientLastName").val("");
-        $("#clientEmail").val("");
-        $("#clientPhoneNumber").val("");
-        $("#emergencyNumber").val("");
-        $("#specialistName").val("");
-        $("#specialistDepartment").val("");
-        $("#visitingDate").val("");
+        $("#appointmentTime").val("");
     };
-    $("button.showBookings").click(function(event) {
-        event.preventDefault();
-        $("form#appointmentForm").slideToggle();
-    });
 
-    $("#bookNow").click(function() {
+
+    $("#bookNow").click(function (event) {
         event.preventDefault();
+        // console.log("Submitted");
+
         let clientFirstname = $("#firstName").val();
         let clientLastname = $("#secondName").val();
         let clientEmail = $("#email").val();
         let clientPhoneNumber = $("#yourNumber").val();
         let emergencyNumber = $("#emergencyContact").val();
-        let specialistName = $("#doctor").val();
         let specialistDepartment = $("#department").val();
-        let visitingDate = $("appointmentDate").val();
+        let specialistName = $("#doctor").val();
+        let visitingDate = $("#appointmentDate").val();
+        let visitingTime = $("#appointmentTime").val();
 
         if (clientFirstname == '' || clientLastname == '' || clientEmail == '' ||
             clientPhoneNumber == '' || emergencyNumber == '' || specialistName == '' ||
-            specialistDepartment == '' || visitingDate == '') {
+            specialistDepartment == '' || visitingDate == '' || visitingTime == '') {
             alert("Please fill all the fields!")
         } else {
-            $("#appointmentForm").show();
 
-            // Appending to the table
-            var tableRow = document.createElement("tr");
+        $("#bookAppointment").show();
 
-            var tabledataFName = document.createElement("td");
-            tabledataFName.innerHTML = clientFirstname;
+        // Appending to the table
+        var tableRow = document.createElement("tr");
 
-            var tabledataLName = document.createElement("td");
-            tabledataLName.innerHTML = clientLastname;
+        var tabledataFName = document.createElement("td");
+        tabledataFName.innerHTML = clientFirstname;
 
-            var tabledataEmail = document.createElement("td");
-            tabledataEmail.innerHTML = clientEmail;
+        var tabledataLName = document.createElement("td");
+        tabledataLName.innerHTML = clientLastname;
 
-            var tabledataPhone = document.createElement("td");
-            tabledataPhone.innerHTML = clientPhoneNumber;
+        var tabledataEmail = document.createElement("td");
+        tabledataEmail.innerHTML = clientEmail;
 
-            var tabledataEmPhone = document.createElement("td");
-            tabledataEmPhone.innerHTML = emergencyNumber;
+        var tabledataPhone = document.createElement("td");
+        tabledataPhone.innerHTML = clientPhoneNumber;
 
-            var tabledataSpecialist = document.createElement("td");
-            tabledataSpecialist.innerHTML = specialistName;
+        var tabledataEmPhone = document.createElement("td");
+        tabledataEmPhone.innerHTML = emergencyNumber;
+        
+        var tabledataSpecialist = document.createElement("td");
+        tabledataSpecialist.innerHTML = specialistName;
 
-            var tabledataSpecialistDepartment = document.createElement("td");
-            tabledataSpecialistDepartment.innerHTML = specialistDepartment;
+        var tabledataSpecialistDepartment = document.createElement("td");
+        tabledataSpecialistDepartment.innerHTML = specialistDepartment;
 
-            var tabledatavisitDate = document.createElement("td");
-            tabledatavisitDate.innerHTML = visitingDate;
+        var tabledatavisitDate = document.createElement("td");
+        tabledatavisitDate.innerHTML = visitingDate;
+
+        var tabledatavisitTime = document.createElement("td");
+        tabledatavisitTime.innerHTML = visitingTime;
 
 
-            tableRow.appendChild(tabledataFName);
-            tableRow.appendChild(tabledataLName);
-            tableRow.appendChild(tabledataEmail);
-            tableRow.appendChild(tabledataPhone);
-            tableRow.appendChild(tabledataEmPhone);
-            tableRow.appendChild(tabledataSpecialist);
-            tableRow.appendChild(tabledataSpecialistDepartment);
-            tableRow.appendChild(tabledatavisitDate);
+        tableRow.appendChild(tabledataFName);
+        tableRow.appendChild(tabledataLName);
+        tableRow.appendChild(tabledataEmail);
+        tableRow.appendChild(tabledataPhone);
+        tableRow.appendChild(tabledataEmPhone);
+        tableRow.appendChild(tabledataSpecialist);
+        tableRow.appendChild(tabledataSpecialistDepartment);
+        tableRow.appendChild(tabledatavisitDate);
+        tableRow.appendChild(tabledatavisitTime);
 
-            $("#bookAppointmentBody").append(tableRow);
+        $("#bookAppointmentBody").append(tableRow);
 
-            resetValues();
+        resetValues();
 
         }
     });
